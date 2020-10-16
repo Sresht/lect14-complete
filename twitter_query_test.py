a9d7a7e1618ce29e0ad7a433c41876714f82507b
EC2 Default User <ec2-user@ip-172-31-12-7.ec2.internal>
2020-10-16 02:33:35 +0000

    Use mocked search response

--- twitter_query_test.py
+++ twitter_query_test.py
@@ -14,13 +14,22 @@ import unittest.mock as mock
 from dotenv import load_dotenv
 import tweepy
 import os
+import datetime
 from os.path import join, dirname
 
 KEY_INPUT = "input"
 KEY_EXPECTED = "expected"
 
-dotenv_path = join(dirname(__file__), 'tweepy.env')
-load_dotenv(dotenv_path)
+class MockedAuthorResponse:
+    def __init__(self, screen_name):
+        self.screen_name = screen_name
+
+class MockedSearchResponse:
+    def __init__(self, text, screen_name, source_url, created_at):
+        self.text = text
+        self.author = MockedAuthorResponse(screen_name)
+        self.source_url = source_url
+        self.created_at = created_at
 
 class TwitterQueryTestCase(unittest.TestCase):
     def setUp(self):
@@ -28,6 +37,7 @@ class TwitterQueryTestCase(unittest.TestCase):
             {
                 KEY_INPUT: "Biggie Smalls",
                 KEY_EXPECTED: {
+                    KEY_CONTENTS: "mocked tweet",
                     KEY_AUTHOR: "@triciascot99",
                     KEY_DATE: "10/15/2020, 21:24",
                     KEY_URL: "http://twitter.com/download/iphone",
@@ -37,24 +47,38 @@ class TwitterQueryTestCase(unittest.TestCase):
         
     def mocked_random_choice(self, values):
         return values[0]
+    
+    def mocked_api_search(self, q, count):
+        return [
+            MockedSearchResponse(
+                "mocked tweet",
+                "triciascot99",
+                "http://twitter.com/download/iphone",
+                datetime.datetime.strptime("10/15/2020, 21:24", "%m/%d/%Y, %H:%M")),
+            MockedSearchResponse(
+                "aslfdjasdf",
+                "sadfasdfas",
+                "asfjkaldfjad",
+                datetime.datetime.strptime("10/19/2020, 21:01", "%m/%d/%Y, %H:%M")),
+            ]
 
     def test_get_random_tweet_success(self):
         for test_case in self.success_test_params:
-            relevant_tweets = get_relevant_tweets(
-                query = test_case[KEY_INPUT],
-                access_token = TWITTER_ACCESS_TOKEN,
-                access_token_secret = TWITTER_ACCESS_TOKEN_SECRET,
-                key = TWITTER_KEY,
-                key_secret = TWITTER_KEY_SECRET,
-                count = 3)
+            with mock.patch('tweepy.API.search', self.mocked_api_search):
+                relevant_tweets = get_relevant_tweets(
+                    query = test_case[KEY_INPUT],
+                    access_token = TWITTER_ACCESS_TOKEN,
+                    access_token_secret = TWITTER_ACCESS_TOKEN_SECRET,
+                    key = TWITTER_KEY,
+                    key_secret = TWITTER_KEY_SECRET,
+                    count = 3)
                 
             with mock.patch('random.choice', self.mocked_random_choice):
                 random_tweet = get_random_tweet(relevant_tweets)
                 
             expected = test_case[KEY_EXPECTED]
             
-            self.assertEqual(random_tweet[KEY_AUTHOR], expected[KEY_AUTHOR])
-            self.assertEqual(random_tweet[KEY_DATE], expected[KEY_DATE])
+            self.assertDictEqual(random_tweet, expected)
         
 if __name__ == '__main__':
     unittest.main()
